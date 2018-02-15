081adf4089b78960f31aca3c83ac44b96eb539b3
agodoriru <takumijohn0806@gmail.com>
2018-02-15 16:44:35 +0900

    add open_tab_func

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sublime
 import sublime_plugin
 
+import webbrowser
+
 def MakeURL(str):
     url='https://google.com/search?q=%s&?oq=%s/'%(str,str)
     return url
@@ -10,4 +12,5 @@ class SearchGoogleCommand(sublime_plugin.TextCommand):
         str_location=self.view.sel()          #get string location(list)
         str=self.view.substr(str_location[0]) #get string
         url=MakeURL(str)                      #make URL
-        sublime.message_dialog(url)           #show URL
+        # sublime.message_dialog(url)           #show URL
+        webbrowser.open_new_tab(url)         #open tab
