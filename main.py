4582dc84c5c5bd60a6cb117c992b88aca05c5e12
agodoriru <takumijohn0806@gmail.com>
2018-02-15 16:41:47 +0900

    add get_URL_func

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import sublime
 import sublime_plugin
 
+def MakeURL(str):
+    url='https://google.com/search?q=%s&?oq=%s/'%(str,str)
+    return url
+
 class SearchGoogleCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        self.view.insert(edit, 0, "Hello, World!")
+        str_location=self.view.sel()          #get string location(list)
+        str=self.view.substr(str_location[0]) #get string
+        url=MakeURL(str)                      #make URL
+        sublime.message_dialog(url)           #show URL
